4a155f144e0e459d96e55541756919c3fddb1158
qwer030413 <comosjay@gmail.com>
2023-10-17 16:45:29 -0700

    little prog

---
+++ Project/indexing.js
@@ -0,0 +1,11 @@
+function pressed(){
+    console.log("Pressed");
+    // assign the value that I put in into imp which is the text box thing
+    let inp = document.getElementById("task");
+    console.log(inp.value);
+    
+    
+
+    
+
+}
