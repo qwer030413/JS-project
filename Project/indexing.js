c67bce77fa2868d6d288265e317289d34a259a31
Chris <comosjay@gmail.com>
2023-10-23 23:34:39 -0700

    assd

--- Project/indexing.js
+++ Project/indexing.js
@@ -189,32 +189,28 @@ if(lb)
 }
 
 
-const user = auth.currentUser;
 onAuthStateChanged(auth, (user) => {
-  if (user) {
-    // User is signed in, see docs for a list of available properties
-    // https://firebase.google.com/docs/reference/js/auth.user
-    if (user !== null) {
-        // The user object has basic properties such as display name, email, etc.
-        const displayName = user.displayName;
-        const email = user.email;
-        const photoURL = user.photoURL;
-        const emailVerified = user.emailVerified;
-    
-        // The user's ID, unique to the Firebase project. Do NOT use
-        // this value to authenticate with your backend server, if
-        // you have one. Use User.getToken() instead.
-        const uid = user.uid;
-    
-        document.getElementById("status").textContent = "Welcome " + email;
-        }
-    const uid = user.uid;
-    // ...
-  } else {
-    document.getElementById("status").textContent = "Guest";
-    // User is signed out
-    // ...
-  }
-});
+    if (user) {
+        // if(auth.curretUser !== null){
+        //     document.getElementById("status").textContent = "Welcome ";
+
+        // }
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/auth.user
+
+      const uid = user.uid;
+
+      // ...
+    } else if(user == null) {
+        if(auth.curretUser == null){
+            document.getElementById("status").textContent = "Guest";
+        }   
+        
+
+      // User is signed out
+      // ...
+    }
+  });
+
 
     
