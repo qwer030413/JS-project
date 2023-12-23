9c555c31fcee93fe4aea8c6810b7f55c87da7028
Chris <comosjay@gmail.com>
2023-12-23 04:29:25 -0800

    rahh

--- Project/indexing.js
+++ Project/indexing.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
 import { getDatabase , onValue,set, ref, update, push, remove } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-database.js";
 import { getAuth, sendEmailVerification, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut   } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-auth.js";
-// import {getFirestore, collection} from "firebase/firestore";
+import {getFirestore, collection, getDocs, addDoc, setDoc, doc, getDoc} from "https://www.gstatic.com/firebasejs/10.5.0/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,15 +17,16 @@ messagingSenderId: "720665752666",
 appId: "1:720665752666:web:84c6ee49bac7cade571463",
 measurementId: "G-PPWBZ8WQK7"
 };
-//TODO: finished storing data in fire base.
-//need to figure out how to read and iterate data and show it on list if logged in
-//idk what I did, good luck understanding what I did
+//TODO: firestore is the way
+//storing in firestore is done but I am trying to read it and it keeps giving me quiry thing
+//we are working on the onauthchange method
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 const auth = getAuth();
-// const fs = getFirestore();
-// const colRef = collection(fs, 'test');
+const fs = getFirestore(app);
+var curuser;
 
 
 
@@ -34,65 +35,34 @@ let input = document.getElementById("input");
 let description = document.getElementById("description");
 //asign the list to the list 
 let list = document.getElementById("list");
+let liststore = []
 
+//fetching data
+// const colRef = doc(fs, 'test', 'jVRZTEjKvTexdEmBXXxI');
+
+// getDocs(colRef)
+//     .then((snapshot) => {
+//         let books = []
+//         snapshot.docs.forEach((doc) => {
+//             books.push({...doc.data(), id: doc.id})
+//         })
+//         console.groupCollapsed(books)
+//     })
+    
+//writing data
+// const addinfo = document.querySelector('.add')
+// addinfo.addEventListener('submit' ,(e) => {
+
+//     e.preventDefault()
+
+//     addDoc(colRef, {
+
+//     })
+
+    
+// })
 // function runs when button clicked
-window.add = function add(){
 
-    if(input.value == '' || description.value =='')
-    {
-        alert("Nuh uh");
-    }
-    else{
-        //create new element for list
-        let li = document.createElement("li");
-        
-
-        
-        li.setAttribute('style', 'white-space: pre;');
-        //assign the value of the input into the li value
-        li.textContent = input.value + "\r\n" + description.value;
-        const todo = li.textContent;
-        //get user id and current user
-        onAuthStateChanged(auth, (user) => {
-            //if logged in
-            if (user) {
-                //get user id
-                const uid = user.uid;
-                
-                //get a ref and set the folder locatioin
-                const postListRef = ref(database, 'users/' + uid + '/todo/');
-                
-                //push the info to the folder
-                const newPostRef = push(postListRef);
-                
-                //setting the newpost ref, which is postlistref being pushed
-                set(newPostRef, {
-                    todo 
-                });
-              
-              // ...
-            } else {
-              
-            }
-          });
-        
-            
-        
-        
-        //add the text node to the newly created element
-        list.appendChild(li);
-        // list.appendChild(de);
-        //span is a given/set value in the code(I didnt do anything with span)
-        let span = document.createElement("span");
-        //also a given thing, set span as x icon(the weird code is for the x icon, it is built in)
-        span.textContent = "\u00d7";
-        li.appendChild(span);
-        // de.appendChild(span);
-    }
-    //clear the text input after it is entered
-    input.value = "";
-    description.value ="";
-}
 
 
 
@@ -157,7 +127,34 @@ if(su){
         var password = document.getElementById('password').value;
         var username = document.getElementById('name').value;
         createUserWithEmailAndPassword(auth, email, password)
-            .then((userCredential) => {
+            .then(async (userCredential) => {
+                const user = userCredential.user;
+                await setDoc(doc(fs, "users",  user.uid), {
+                    list: liststore
+                });
+                
+                sendEmailVerification(auth.currentUser)
+                .then(() => {
+                alert('Vertification email sent!');
+                // Signed up 
+                
+                set(ref(database, 'users/'+  user.uid),{
+                    profile : username,
+                    password: password,
+                    email: email
+                })
+                alert('user Created!')
+                console.log("signed up");
+            
+
+                // ...
+
+                });
+                    
+                })
+                
+                
+            .then(() => {
                 sendEmailVerification(auth.currentUser)
                 .then(() => {
                 alert('Vertification email sent!');
@@ -174,8 +171,7 @@ if(su){
 
                 // ...
 
-                });;
-                
+                });
             })
             .catch((error) => {
                 const errorCode = error.code;
@@ -223,7 +219,7 @@ if(lb)
     });
 }
 
-onAuthStateChanged(auth, (user) => {
+onAuthStateChanged(auth, async (user) => {
 const button = document.getElementById("log");
 
   if (user) {
@@ -237,36 +233,83 @@ const button = document.getElementById("log");
         const photoURL = user.photoURL;
         const emailVerified = user.emailVerified;
         const uid = user.uid;
+        curuser = uid;
 
         let li = document.createElement("li");
-
-        // db.collection("cities").get().then(function(querySnapshot) {     
-            // querySnapshot.forEach(function(doc) {         
-            // console.log(doc.id, " => ", doc.data());     });
-        const updateList = ref(database, 'users/' + uid + '/todo/')
-                onValue(updateList, (snapshot) => {
-                    const data = snapshot.val();
-                    li.textContent = data.textContent;
-                    list.appendChild(li);
-                    let span = document.createElement("span");
-                    li.appendChild(span);
-
-                })
-    
+        const iterator = liststore.keys();
+        //not working
+        const snapshot = await getDocs(doc(fs, 'users', '9KymWdGBHUYf7EEKOv8mrWVQXzF3'))
+            // const colRef = doc(fs, 'users/9KymWdGBHUYf7EEKOv8mrWVQXzF3');
+            // getDocs(colRef)
+            // .then((snapshot) => {
+            //             let elems = []
+            //             snapshot.docs.forEach((doc) => {
+            //                 elems.push({...doc.data(), id: doc.id})
+            //             })
+            //             console.log(elems)
+            //         })
+        
+        console.log(liststore);
         // The user's ID, unique to the Firebase project. Do NOT use
         // this value to authenticate with your backend server, if
         // you have one. Use User.getToken() instead.
         button.disabled = true;
         document.getElementById("status").textContent = "Welcome " + email;
+
     
-  } else {
+  } 
+  else {
     document.getElementById("status").textContent = "Guest";
     button.disabled = false;
+    curuser = null;
     // User is signed out
     // ...
   }
 }); 
+window.add = async function add(){
 
+    if(input.value == '' || description.value =='')
+    {
+        alert("Nuh uh");
+    }
+    else{
+        //create new element for list
+        let li = document.createElement("li");
+        
+
+        
+        li.setAttribute('style', 'white-space: pre;');
+        //assign the value of the input into the li value
+        li.textContent = input.value + "\r\n" + description.value;
+        liststore.push(li.textContent);
+        if(curuser != null)
+        {
+            await setDoc(doc(fs, "users",  curuser), {
+                list: liststore
+            });
+        }
+        console.log(liststore)
+        // const todo = li.textContent;
+        
+        
+            
+        
+        
+        //add the text node to the newly created element
+        list.appendChild(li);
+        // list.appendChild(de);
+        //span is a given/set value in the code(I didnt do anything with span)
+        let span = document.createElement("span");
+        //also a given thing, set span as x icon(the weird code is for the x icon, it is built in)
+        span.textContent = "\u00d7";
+        li.appendChild(span);
+        // de.appendChild(span);
+    }
+    //clear the text input after it is entered
+    input.value = "";
+    description.value ="";
+    // console.log(liststore);
+}
 
 
 
